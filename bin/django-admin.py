deec1ff26ac7196f773991c3bc3379d97abcb498
James Finucane <jnishiyama@gmail.com>
2013-03-06 08:28:58 -0500

    lhis reverts commit a7809938f23d4540bc65aa2be328f672e40d73ec.

--- bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Applications/MAMP/htdocs/MPharma/MPharma/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- bin/unit2.py
+++
@@ -1,7 +0,0 @@
-#!/Applications/MAMP/htdocs/MPharma/MPharma/bin/python
-
-__unittest = True
-
-from unittest2.main import main_
-
-main_()
